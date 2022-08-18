99d4dff87b41e3b0333b6bb4f5a4b1f067db4b44
AJ <mahidlawonaljun@gmail.com>
2022-08-18 13:25:10 +0800

    Add documentation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Declare variables
 let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
@@ -5,30 +6,38 @@ const tabBtn = document.getElementById('tab-btn');
 const deleteBtn = document.getElementById('delete-btn');
 const ulEl = document.getElementById('ul-el');
 
+//Save whatever is on input field
 inputBtn.addEventListener('click', function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = '';
-  localStorage.setItem('myLeads', JSON.stringify(myLeads));
-  render(myLeads);
+  myLeads.push(inputEl.value); //pushig input value in myleads array
+  inputEl.value = ''; //reseting/emptying input field
+  localStorage.setItem('myLeads', JSON.stringify(myLeads)); //turning myleads value into object string and save it on local storage
+  render(myLeads); //calling render fucmtion with myleads array as an arguement
 });
 
+//Save the current tab and current windows
+//NOTE: only works as extension not in live
 tabBtn.addEventListener('click', () => {
+  //API
+  //Querying chrome tabs
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    myLeads.push(tabs[0].url);
+    myLeads.push(tabs[0].url); //getting the first value of tabs url key
     localStorage.setItem('myLeads', JSON.stringify(myLeads));
     render(myLeads);
   });
 });
 
+//Delete all save link on localstorage, array and DOM
 deleteBtn.addEventListener('dblclick', () => {
   localStorage.clear();
   myLeads = [];
   render(myLeads);
 });
 
+// To render the data/array value in the DOM
 function render(leads) {
   let listItems = '';
   for (let i = 0; i < leads.length; i++) {
+    // assigning the listItem with element li and a then set the lead variable as its value
     listItems += `
             <li>
                 <a target='_blank' href='${leads[i]}'>
@@ -37,12 +46,15 @@ function render(leads) {
             </li>
         `;
   }
-  ulEl.innerHTML = listItems;
+  ulEl.innerHTML = listItems; //make listItems a UL inner HTMl element and value
 }
 
+// GET/FETCH string object myLeads key and its value from localstorage
+// assign it to leadsFromLocalStorage variable
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+// If leadsFromLocalStorage  isn't empty = true
 if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
+  myLeads = leadsFromLocalStorage; // assign it to myLeads array
   render(myLeads);
 }
 
